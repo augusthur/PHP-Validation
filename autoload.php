a37298fb139d314e573e683e570a6d1a7436092e
Augusto Mathurin <augusto@localhost.localdomain>
2014-10-03 16:22:30 -0300

    autoload fixed

--- autoload.php
+++ autoload.php
@@ -1,9 +1,8 @@
 <?php
 
 spl_autoload_register(function($class_name){
-    $file_name = substr($class_name, 21);
-	$file_name = str_replace('\\', '/', $file_name);
+	$file_name = str_replace('\\', '/', $class_name);
 	$file_name = str_replace('_', '/', $file_name);
-	$file = dirname(__FILE__) . "/src/$file_name.php";
+	$file = dirname(__FILE__) . "/$file_name.php";
 	if(is_file($file)) include $file;
 });
